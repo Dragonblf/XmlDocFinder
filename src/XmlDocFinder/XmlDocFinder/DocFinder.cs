e7917ae761e07145aae2895d60fb68a320dcb4ed
Dragonblf <thomas-trautwein@ok.de>
2021-08-25 17:25:35 +0200

    Added base structure of project

---
+++ src/XmlDocFinder/XmlDocFinder/DocFinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dragonblf.XmlDocFinder
+{
+    /// <summary>
+    /// Helper class to find paths to XML documentation files for assemblies.
+    /// </summary>
+    public class DocFinder : IDocFinder
+    {
+        public string FindFor<T>() where T : new() => FindFor(typeof(T).Assembly);
+
+        public bool TryFindFor<T>(out string path) where T : new() => TryFindFor(typeof(T).Assembly, out path);
+
+        public string FindFor(in Assembly assembly)
+        {
+            if (assembly == null) { throw new ArgumentNullException(nameof(assembly)); }
+
+            throw new NotImplementedException();
+        }
+
+        public bool TryFindFor(in Assembly assembly, out string path)
+        {
+            if (assembly == null) { throw new ArgumentNullException(nameof(assembly)); }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/XmlDocFinder/XmlDocFinder/IDocFinder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dragonblf.XmlDocFinder
+{
+    /// <summary>
+    /// Interface which defines helper methods to implement
+    /// to find paths to XML documentation files for assemblies.
+    /// </summary>
+    public interface IDocFinder
+    {
+        /// <summary>
+        /// Finds XML documentation file path for assembly declaring
+        /// <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">Type for which declaring assembly to find the XML documentation file path</typeparam>
+        /// <returns>Path to XML documentation file if path was found else <see cref="string.Empty"/></returns>
+        public string FindFor<T>() where T : new();
+
+        /// <summary>
+        /// Finds XML documentation file path for assembly declaring
+        /// <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">Type for which declaring assembly to find the XML documentation file path</typeparam>
+        /// <param name="path">Path to XML documentation file if path was found else <see cref="string.Empty"/></param>
+        /// <returns>True if path to XML documentation file was found otherwise false</returns>
+        public bool TryFindFor<T>(out string path) where T : new();
+
+        /// <summary>
+        /// Finds XML documentation file path for <paramref name="assembly"/>.
+        /// </summary>
+        /// <param name="assembly">Assembly for which to find its XML documentation file path</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns>Path to XML documentation file if path was found else <see cref="string.Empty"/></returns>
+        public string FindFor(in Assembly assembly);
+
+        /// <summary>
+        /// Finds XML documentation file path for <paramref name="assembly"/>.
+        /// </summary>
+        /// <param name="assembly">Assembly for which to find its XML documentation file path</param>
+        /// <param name="path">Path to XML documentation file if path was found else <see cref="string.Empty"/></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns>True if path to XML documentation file was found otherwise false</returns>
+        public bool TryFindFor(in Assembly assembly, out string path);
+    }
+}

c7a94cdd6533c14a2a21356af64a48f1fe5d2295
Dragonblf <thomas-trautwein@ok.de>
2021-08-25 17:39:09 +0200

    Added base test structure of project

---
+++ tests/XmlDocFinder.Tests/DocFinderTests.cs
@@ -0,0 +1,69 @@
+using Dragonblf.XmlDocFinder;
+using System;
+using Xunit;
+using System.Reflection;
+using FakeItEasy;
+using Shouldly;
+
+namespace XmlDocFinder.Tests
+{
+    public class DocFinderTests
+    {
+        private DocFinder _testClass;
+
+        private Assembly _assembly;
+
+
+        public DocFinderTests()
+        {
+            _testClass = new DocFinder();
+            _assembly = A.Fake<Assembly>();
+        }
+
+
+        [Fact]
+        public void Can_Construct()
+        {
+            var instance = new DocFinder();
+            instance.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void Call_FindForT_WithType_NotImplementedException()
+        {
+            Should.Throw<NotImplementedException>(() => _testClass.FindFor<object>());
+        }
+
+        [Fact]
+        public void Call_TryFindForT_WithType_NotImplementedException()
+        {
+            Should.Throw<NotImplementedException>(() => _testClass.TryFindFor<object>(out var path));
+        }
+
+        [Fact]
+        public void Call_FindFor_WithAssembly_NotImplementedException()
+        {
+            Should.Throw<NotImplementedException>(() => _testClass.FindFor(_assembly));
+        }
+
+        [Fact]
+        public void Call_FindFor_WithNull_ArgumentNullException()
+        {
+            Assembly assembly = null;
+            Should.Throw<ArgumentNullException>(() => _testClass.FindFor(assembly));
+        }
+
+        [Fact]
+        public void Call_TryFindFor_WithAssembly_NotImplementedException()
+        {
+            Should.Throw<NotImplementedException>(() => _testClass.TryFindFor(_assembly, out var path));
+        }
+
+        [Fact]
+        public void Call_TryFindFor_WithNull_ArgumentNullException()
+        {
+            Assembly assembly = null;
+            Should.Throw<ArgumentNullException>(() => _testClass.TryFindFor(assembly, out var path));
+        }
+    }
+}

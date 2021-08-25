5871afcfb82d5386953ae3589941e813f4071242
Dragonblf <thomas-trautwein@ok.de>
2021-08-25 17:54:04 +0200

    Added simple dependency injection functionality

---
+++ src/XmlDocFinder/XmlDocFinder/DI/Bootstrap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleInjector;
+
+namespace Dragonblf.XmlDocFinder.DI
+{
+    /// <summary>
+    /// Contains the dependency injection bootstrap for library.
+    /// </summary>
+    internal static class Bootstrap
+    {
+        /// <summary>
+        /// Apply dependency injection for the base project.
+        /// </summary>
+        /// <param name="container">Dependency injection container to use</param>
+        /// <returns>Dependency injection container</returns>
+        internal static Container Initialize(this Container container)
+        {
+            return container;
+        }
+    }
+}
---
+++ src/XmlDocFinder/XmlDocFinder/DI/DIProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleInjector;
+
+namespace Dragonblf.XmlDocFinder.DI
+{
+    /// <summary>
+    /// Provides simple dependency injection functionality.
+    /// </summary>
+    internal static class DIProvider
+    {
+        /// <summary>
+        /// Holds the dependency injection container.
+        /// </summary>
+        private static readonly Container Container;
+
+
+        /// <summary>
+        /// Initializes functionality for <see cref="DIProvider"/>.
+        /// </summary>
+        static DIProvider()
+        {
+            // Create injector container
+            Container = new Container();
+
+            // Initialize dependency injection data
+            Container.Initialize();
+        }
+
+
+        /// <summary>
+        /// Gets an instance of given type <see cref="T"/>.
+        /// </summary>
+        /// <typeparam name="T">Interface to find</typeparam>
+        /// <returns>Instance</returns>
+        public static T GetInstance<T>() where T : class
+        {
+            return Container.GetInstance<T>();
+        }
+    }
+}
